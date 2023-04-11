ba1e9a1e4de7460530f2b9109679259c84a9e782
Tiago Dalmolin <tiagodalmolin028@gmail.com>
2023-04-10 23:51:58 -0300

    Game Super Mario

---
+++ js/script.js
@@ -0,0 +1,11 @@
+ const mario = document.querySelector('.mario');
+
+ const jump = () =>{
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    },800);
+ }
+ 
+ document.addEventListener('keydown', jump);
